3068fb3a80c96fe5a27b1341eb6e17b330d35028
hitesh <hiteshr@zignuts.com>
2023-03-07 16:00:14 +0530

    changes on ModuleController in create and update method for validation, changed functionality

--- expence_manager/app/Http/Controllers/ModulePermissionController.php
+++ expence_manager/app/Http/Controllers/ModulePermissionController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\ModulePermission;
+use App\Models\Permission;
 use App\Traits\ResponseMessage;
 use GrahamCampbell\ResultType\Success;
 use Illuminate\Http\Request;
@@ -25,37 +26,64 @@ class ModulePermissionController extends Controller
     public function create(Request $request)
     {
         $validate_module_permission = Validator::make($request->all(), [
-            'add_access'    => 'required|boolean|numeric|max:1',
-            'edit_access'   => 'required|boolean|numeric|max:1',
-            'view_access'   => 'required|boolean|numeric|max:1',
-            'delete_access' => 'required|boolean|numeric|max:1',
-            'module_code'   => 'required|alpha_dash|exists:modules,code',
-            'permission_id' => 'required|numeric|exists:permissions,id'
+            'module_code'   => 'required|alpha_dash|exists:modules,code|unique:module_permissions,module_code',
+            'permission_id' => 'required|numeric|exists:permissions,id|unique:module_permissions,permission_id'
         ]);
 
         if ($validate_module_permission->fails()) {
             return $this->ErrorResponse($validate_module_permission);
         }
 
-        $module_permission = ModulePermission::create($request->only(
-            'add_access',
-            'edit_access',
-            'view_access',
-            'delete_access',
-            'module_code',
-            'permission_id'
-        ));
+        $permission = Permission::find($request->permission_id);
 
-        return $this->success('Module Permission Created Successfuly', $module_permission);
+        if ($permission->permission_name == 'create') {
+            $module_permission = ModulePermission::create(
+                $request->only('module_code', 'permission_id') +
+                    [
+                        'add_access' => true,
+                    ]
+            );
+            return $this->success('Module Permission Created Successfuly', $module_permission);
+        }
+
+        if ($permission->permission_name == 'update') {
+            $module_permission = ModulePermission::create(
+                $request->only('module_code', 'permission_id') +
+                    [
+                        'edit_access' => true,
+                    ]
+            );
+            return $this->success('Module Permission Created Successfuly', $module_permission);
+        }
+
+        if ($permission->permission_name == 'delete') {
+            $module_permission = ModulePermission::create(
+                $request->only('module_code', 'permission_id') +
+                    [
+                        'delete_access' => true,
+                    ]
+            );
+            return $this->success('Module Permission Created Successfuly', $module_permission);
+        }
+
+        if ($permission->permission_name == 'view') {
+            $module_permission = ModulePermission::create(
+                $request->only('module_code', 'permission_id') +
+                    [
+                        'view_access' => true,
+                    ]
+            );
+            return $this->success('Module Permission Created Successfuly', $module_permission);
+        }
     }
 
     public function update(Request $request, $id)
     {
         $validate_module_permission = Validator::make($request->all(), [
-            'add_access'    => 'required|boolean|numeric|max:1',
-            'edit_access'   => 'required|boolean|numeric|max:1',
-            'view_access'   => 'required|boolean|numeric|max:1',
-            'delete_access' => 'required|boolean|numeric|max:1',
+            'add_access'    => 'boolean|numeric|max:1',
+            'edit_access'   => 'boolean|numeric|max:1',
+            'view_access'   => 'boolean|numeric|max:1',
+            'delete_access' => 'boolean|numeric|max:1',
         ]);
 
         if ($validate_module_permission->fails()) {
